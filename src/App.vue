5fe185ad56b032cf093a320503d7fbcc7e063ae4
Enpas <enniopas2002@libero.it>
2021-05-19 15:49:06 +0200

    completo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
+    <Title />
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Title from './components/Title.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
 
 <style lang="scss">
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +31,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <header>
+    <h1>La mia prima app con Vue CLI!</h1>
+  </header>
+  
+</template>
+
+<script>
+export default {
+
+  name: 'Title'
+    
+}
+</script>
+
+<style scoped>
+
+header {
+  margin: 0 0 50px 0;
+}
+
+h1 {
+  font-size: 50px;
+}
+
+</style>
